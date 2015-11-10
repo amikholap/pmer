44f0b9b4b5fa6632246ba235dcf21cc0b0adfbaa
amikholap <ales.mikholap@gmail.com>
2015-11-10 20:36:47 +0300

    Basic Elo implementation

---
+++ pmer/base.py
@@ -0,0 +1,44 @@
+import collections
+
+
+class Event(object):
+
+    def __init__(self, winners, losers, date=None):
+        self.winners = winners
+        self.losers = losers
+        self.date = date
+
+
+class Rating(object):
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Rater(object):
+
+    _rating_class = Rating
+
+    def __init__(self, *, initial_rating_value=1):
+        self._initial_rating_value = initial_rating_value
+        self._ratings = collections.defaultdict(self._init_rating)
+
+    def __getitem__(self, key):
+        return self._ratings[key]
+
+    def _init_rating(self):
+        initial_params = self._get_initial_rating_params()
+        rating = self._rating_class(**initial_params)
+        return rating
+
+    def _get_initial_rating_params(self):
+        params = {
+            'value': self._initial_rating_value,
+        }
+        return params
+
+    def get_win_probabilities(self, team_a, team_b):
+        raise NotImplementedError
+
+    def update_ratings(self, event):
+        raise NotImplementedError
---
+++ pmer/elo.py
@@ -0,0 +1,46 @@
+import math
+from .base import Rater
+
+
+class EloRater(Rater):
+
+    def __init__(self, *, K=0.1, scale=0.5, **kwargs):
+        super().__init__(**kwargs)
+        self.K = K
+        self.scale = scale
+
+    def get_win_probabilities(self, team_a, team_b):
+        assert len(team_a) == len(team_b)
+
+        team_a_rating = self._get_team_rating(team_a)
+        team_b_rating = self._get_team_rating(team_b)
+
+        team_a_pwin, team_b_pwin = self._get_win_probabilities_for_ratings(team_a_rating, team_b_rating)
+
+        return team_a_pwin, team_b_pwin
+
+    def _get_team_rating(self, team):
+        team_rating_sum = sum([self[player_id].value for player_id in team])
+        return team_rating_sum
+
+    def _get_win_probabilities_for_ratings(self, rating_a, rating_b):
+        a_pwin = 1 / (1 + math.exp((rating_b - rating_a) / self.scale))
+        b_pwin = 1 - a_pwin
+        return a_pwin, b_pwin
+
+    def update_ratings(self, event):
+        assert len(event.winners) == len(event.losers)
+
+        winners_rating = self._get_team_rating(event.winners)
+        losers_rating = self._get_team_rating(event.losers)
+
+        winners_pwin, losers_pwin = self._get_win_probabilities_for_ratings(winners_rating, losers_rating)
+
+        delta = self.K * (1 - winners_pwin)
+
+        # Update rating values.
+        # Higher relative rating causes higher update.
+        for player_id in event.winners:
+            self[player_id].value += delta * (self[player_id].value / winners_rating)
+        for player_id in event.losers:
+            self[player_id].value -= delta * (self[player_id].value / losers_rating)
