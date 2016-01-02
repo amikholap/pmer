68b60750b08c9284d291c1b8c2205b489a1136f7
amikholap <ales.mikholap@gmail.com>
2016-01-02 13:27:09 +0300

    Lol and Soccer datasets stored using git-lfs

--- pmer/datasets/__init__.py
+++ pmer/datasets/__init__.py
@@ -1,2 +1,4 @@
 # pylint: disable=unused-import
 from .dota2 import Dota2Dataset
+from .lol import LolDataset
+from .soccer import SoccerDataset
---
+++ pmer/datasets/soccer.py
@@ -0,0 +1,5 @@
+from .base import BaseDataset
+
+
+class SoccerDataset(BaseDataset):
+    pass
