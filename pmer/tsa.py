8ef6a27a5273bce5651a73a1196876a1980b8e27
amikholap <ales.mikholap@gmail.com>
2016-01-11 21:04:27 +0300

    Grid plots for TSA

--- pmer/tsa.py
+++ pmer/tsa.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats
@@ -18,38 +19,49 @@ def diff(ts):
     return ts[1:] - ts[:-1]
 
 
-def plot_ts(ts):
-    plt.figure()
-    plt.plot(ts)
-    plt.title('RAW')
+def plot_ts(xs, ts, title=None, ax=None):
+    if ax is None:
+        ax = plt.axes()
+    ax.plot(xs, ts)
+    ax.xaxis.set_major_locator(matplotlib.dates.YearLocator())
+    if title:
+        ax.set_title(title)
 
 
-def plot_kde(ts):
+def plot_kde(ts, title=None, ax=None):
     """Display kernel density estimation."""
+    if ax is None:
+        ax = plt.axes()
     ts = np.asarray(ts)
     xs = np.linspace(ts.min(), ts.max(), 1000)
     ys = scipy.stats.gaussian_kde(ts).pdf(xs)
-    plt.figure()
-    plt.plot(xs, ys)
-    plt.title('Kernel Density Estimation')
+    ax.plot(xs, ys)
+    if title:
+        ax.set_title(title)
 
 
-def plot_acf(ts, nlags=10):
-    _plot_cf(ts, acf, nlags)
+def plot_acf(ts, nlags=10, title=None, ax=None):
+    if ax is None:
+        ax = plt.axes()
+    _plot_cf(ax, ts, acf, nlags)
+    if title:
+        ax.set_title(title)
 
 
-def plot_pacf(ts, nlags=10):
-    _plot_cf(ts, pacf, nlags)
+def plot_pacf(ts, nlags=10, title=None, ax=None):
+    if ax is None:
+        ax = plt.axes()
+    _plot_cf(ax, ts, pacf, nlags)
+    if title:
+        ax.set_title(title)
 
 
-def _plot_cf(ts, cf_func, nlags):
+def _plot_cf(ax, ts, cf_func, nlags):
     """Helper function to display acf/pacf."""
     ts_cf = cf_func(ts, nlags=nlags)
     xs = np.arange(0, nlags+1, 1)
-    plt.figure()
-    _, stemlines, baseline = plt.stem(xs, ts_cf)
+    _, stemlines, baseline = ax.stem(xs, ts_cf)
     plt.setp(stemlines, 'color', 'b', 'linestyle', '--', 'linewidth', 1)
     plt.setp(baseline, 'visible', False)
-    plt.xticks(np.arange(0, xs[-1] + 1))
-    plt.ylim(0, 1.1)
-    plt.title(cf_func.__name__.upper())
+    ax.set_xticks(np.arange(0, xs[-1] + 1))
+    ax.set_ylim(0, 1.1)
